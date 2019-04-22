7c61af104b8cc199bc1b16108e4658a859cc38a2
NickBohannan <34175376+NickBohannan@users.noreply.github.com>
2019-04-22 16:01:35 -0500

    started code for R->num array

--- main.js
+++ main.js
@@ -1,68 +1,81 @@
 function computeRouterCode(str) {
-    let arrStr = str.split("")
+  let arrStr = str.split("")
+  if (str.charCodeAt(0) < 58 && str.charCodeAt(0) > 48) {
     switch (str[0]) {
-        case "1":
-            if (arrStr[3] === "m" || arrStr[3] === "M") {
-            	arrStr[3] = "H"
-            } else if (arrStr[3] === "h" || arrStr[3] === "H") {
-            	arrStr[3] = "X"
-            } else {
-            	arrStr[3] = String.fromCharCode(str.charCodeAt([3]) + 1)
-            }
-            arrStr[0] = "R"
-            break
-        case "2":
-        		if (arrStr[3] === "m" || arrStr[3] === "M") {
-            	arrStr[3] = "X"
-            } else if (arrStr[3] === "l" || arrStr[3] === "L") {
-            	arrStr[3] = "H"
-            } else {
-            arrStr[3] = String.fromCharCode(str.charCodeAt([3]) + 2)
-            }
-            arrStr[0] = "R"
-            break
-        case "3":
-            if (arrStr[3] === "x" || arrStr[3] === "X") {
-            	arrStr[3] = "H"
-            } else if (arrStr[3] === "h" || arrStr[3] === "H") {
-            	arrStr[3] = "M"
-            } else {
-            arrStr[3] = String.fromCharCode(str.charCodeAt([3]) - 1)
-            }
-            arrStr[0] = "R"
-            break
-        case "4":
-            if (arrStr[3] === "x" || arrStr[3] === "X") {
-            	arrStr[3] = "M"
-            } else if (arrStr[3] === "h" || arrStr[3] === "H") {
-            	arrStr[3] = "L"
-            } else {
-            	arrStr[3] = String.fromCharCode(str.charCodeAt([3]) - 2)
-            }
-            arrStr[0] = "R"
-            break
-        case "5":
-            arrStr[0] = "L"
-            break
-        case "6":
-            arrStr[1] = String.fromCharCode(str.charCodeAt([1]) + 1)
-            arrStr[0] = "R"
-            break
-        case "7":
-            arrStr[1] = String.fromCharCode(str.charCodeAt([1]) - 1)
-            arrStr[0] = "R"
-            break
-        case "8":
-            arrStr[2] = String.fromCharCode(str.charCodeAt([2]) + 1)
-            break
-        case "9":
-            arrStr[2] = String.fromCharCode(str.charCodeAt([2]) + 1)
-            arrStr[0] = "R"
-            break
-        default:
-            alert("Invalid Code")
+      case "1":
+        if (arrStr[3] === "m" || arrStr[3] === "M") {
+          arrStr[3] = "H"
+        } else if (arrStr[3] === "h" || arrStr[3] === "H") {
+          arrStr[3] = "X"
+        } else {
+          arrStr[3] = String.fromCharCode(str.charCodeAt([3]) + 1)
+        }
+        arrStr[0] = "R"
+        break
+      case "2":
+        if (arrStr[3] === "m" || arrStr[3] === "M") {
+          arrStr[3] = "X"
+        } else if (arrStr[3] === "l" || arrStr[3] === "L") {
+          arrStr[3] = "H"
+        } else {
+          arrStr[3] = String.fromCharCode(str.charCodeAt([3]) + 2)
+        }
+        arrStr[0] = "R"
+        break
+      case "3":
+        if (arrStr[3] === "x" || arrStr[3] === "X") {
+          arrStr[3] = "H"
+        } else if (arrStr[3] === "h" || arrStr[3] === "H") {
+          arrStr[3] = "M"
+        } else {
+          arrStr[3] = String.fromCharCode(str.charCodeAt([3]) - 1)
+        }
+        arrStr[0] = "R"
+        break
+      case "4":
+        if (arrStr[3] === "x" || arrStr[3] === "X") {
+          arrStr[3] = "M"
+        } else if (arrStr[3] === "h" || arrStr[3] === "H") {
+          arrStr[3] = "L"
+        } else {
+          arrStr[3] = String.fromCharCode(str.charCodeAt([3]) - 2)
+        }
+        arrStr[0] = "R"
+        break
+      case "5":
+        arrStr[0] = "L"
+        break
+      case "6":
+        arrStr[1] = String.fromCharCode(str.charCodeAt([1]) + 1)
+        arrStr[0] = "R"
+        break
+      case "7":
+        arrStr[1] = String.fromCharCode(str.charCodeAt([1]) - 1)
+        arrStr[0] = "R"
+        break
+      case "8":
+        arrStr[2] = String.fromCharCode(str.charCodeAt([2]) + 1)
+        break
+      case "9":
+        arrStr[2] = String.fromCharCode(str.charCodeAt([2]) + 1)
+        arrStr[0] = "R"
+        break
+      default:
+        alert("Invalid Code")
     }
     return arrStr
+  } else if (str.charCodeAt(0) === "r" || str.charCodeAt(0) === "R") {
+  		let codeArray = []
+      codeArray.push("1"+x)
+      codeArray.push("2"+x)
+      codeArray.push("3"+x)
+      codeArray.push("4"+x)
+      codeArray.push("6"+x)
+      codeArray.push("7"+x)
+      codeArray.push("8"+x)
+      codeArray.push("9"+x)
+      return codeArray
+  }
 }
 
 let sub = document.querySelector('#sub')
